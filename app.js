c3721afc0af6b848cba15268c442370e12c47a95
JAYA CHEDUMBARUM PILLAY <jayavcp@gmail.com>
2024-03-29 01:00:45 +0000

    cookie clicker almost finished. submit btn to be added

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
-console.log("Test");
+console.log("Hello World");
+
 const cookieBtn = document.getElementById("cookieBtn");
+const imgBtn = document.querySelector("img");
+const upgradeBtn = document.getElementById("upgradeBtn");
+const upgradeCost = document.getElementById("upgradeCost");
+const cookiesSpan = document.getElementById("cookiesSpan");
+const cpsSpan = document.getElementById("cpsSpan");
 
-let count = 0;
+// default starting value for stats
+const stats = {
+  cookieCount: 0,
+  cps: 0, //cookies automatically received per second
+};
 
-cookieBtn.addEventListener("click", f(){
-count = count + 1;
-});
+//if local storage exists, update stats with it
+const storageStats = JSON.parse(localStorage.getItem("stats"));
+
+if (storageStats !== null) {
+  stats.cookieCount = storageStats.cookieCount;
+  stats.cps = storageStats.cps;
+  updatePage();
+}
+
+function buyCookie() {
+  stats.cookieCount++;
+  updatePage();
+  updateStorage();
+}
+
+function buyUpgrade() {
+  stats.cookieCount = stats.cookieCount - stats.cps * 5;
+  stats.cps++;
+  updatePage();
+  updateStorage();
+}
+
+function updatePage() {
+  cookiesSpan.textContent = stats.cookieCount;
+  cpsSpan.textContent = stats.cps;
+  upgradeCost.textContent = stats.cps * 5;
+}
+
+function updateStorage() {
+  localStorage.setItem("stats", JSON.stringify(stats));
+}
+
+cookieBtn.addEventListener("click", buyCookie);
+imgBtn.addEventListener("click", buyCookie);
+upgradeBtn.addEventListener("click", buyUpgrade);
+
+// start the timer that runs every second forever
+setInterval(function () {
+  stats.cookieCount += stats.cps;
+  updatePage();
+  updateStorage();
+}, 1000);
